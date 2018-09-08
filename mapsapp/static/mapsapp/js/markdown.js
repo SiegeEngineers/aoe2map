2141f9c90bd01fa1c2e1c7c0d35982e4dba2dbe6
HSZemi <hszemi@users.noreply.github.com>
2018-09-09 01:12:13 +0200

    Render map information text as markdown client-side

---
+++ mapsapp/static/mapsapp/js/markdown.js
@@ -0,0 +1,19 @@
+var md = window.markdownit({breaks: true})
+    .disable(['link', 'image']);
+$('.markdown-text').each(
+    function (nr, it) {
+        let it_select = $(it);
+        it_select.html(
+            md.render(it_select.text())
+                .replace(/<h5/g, "<h6")
+                .replace(/<\/h5/g, "</h6")
+                .replace(/<h4/g, "<h6")
+                .replace(/<\/h4/g, "</h6")
+                .replace(/<h3/g, "<h5")
+                .replace(/<\/h3/g, "</h5")
+                .replace(/<h2/g, "<h4")
+                .replace(/<\/h2/g, "</h4")
+                .replace(/<h1/g, "<h3")
+                .replace(/<\/h1/g, "</h3")
+        );
+    });
